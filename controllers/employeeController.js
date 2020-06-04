b99ef85dd68d3e691532694d13b0dbf5fb549bee
nelkon <nelconcr@gmail.com>
2020-06-04 22:19:20 +0530

    DB
    MongoDB connected

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 
 router.get('/',(req,res) => {
-    res.json('Sample text');
+    res.render("employee/addOrEdit",{
+        viewTitle:"Insert Employee"
+    });
 });
 module.exports=router;
